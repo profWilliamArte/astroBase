11c6f2f6e13f3f3ba51cea52091fb2581154b20a
profWilliamArte <arteagawilliam13@hotmail.com>
2024-02-06 17:27:57 -0400

    7do commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,5 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     site: 'https://profwilliamarte.github.io',
-    base: '/astroBase',
+    base: 'https://profwilliamarte.github.io/astroBase',
 });
